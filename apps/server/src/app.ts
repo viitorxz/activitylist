8e41e88eb8b05943ea750c3db65636d3c43404c6
viitorxz <jvitorsantana@icloud.com>
2023-04-15 14:57:03 -0300

    chore: corringindo bugs

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -4,14 +4,18 @@ import { routes } from './routes'
 
 async function bootstrapt() {
   const app = Fastify()
-  app.register(fastifyCors)
+  app.register(fastifyCors, {
+    origin: ['*'],
+  })
   app.register(routes)
 
   app
     .listen({
       port: 3001,
     })
-    .then(() => console.log(`Running on port ${3001}`))
+    .then(() =>
+      console.log(`Running on port ${3001} [${app.server.address()}]`),
+    )
 }
 
 bootstrapt()
